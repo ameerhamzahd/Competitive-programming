6fa45320197e0ee96559876a5beb17718cae1261
Ameer Hamzah Daiyan <133146759+ameerhamzahd@users.noreply.github.com>
2024-02-05 23:13:49 +0600

    Add files via upload

---
+++ pi.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,i;
+    cin >> n;
+    string s="3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
+
+    for(i = 0; i < (n + 2); i++)
+        cout << s[i];
+
+    cout << endl;
+
+    return 0;
+}
