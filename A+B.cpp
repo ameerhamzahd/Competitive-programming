34698d2b5add57bc127d91d7ccc1d623deba04e7
Ameer Hamzah Daiyan <133146759+ameerhamzahd@users.noreply.github.com>
2024-02-08 18:20:23 +0600

    Add files via upload

---
+++ A+B.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, i, n, x;
+    cin >> n;
+
+    for(i = 0; i < n; i++)
+    {
+        cin >> a >> b;
+
+         x = a + b;
+
+         cout << x << endl;
+    }
+
+    return 0;
+}
+//08:18.04 03/08/2023 implementation 800
