2b52b739871021439569d156f73171233c716b8a
Ameer Hamzah Daiyan <133146759+ameerhamzahd@users.noreply.github.com>
2024-02-26 19:20:05 +0600

    Add files via upload

---
+++ The Ultimate Square.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        double n;
+        long long x;
+        cin >> n;
+
+        x = ceil(n / 2);
+
+        cout << x << endl;
+    }
+
+    return 0;
+}
