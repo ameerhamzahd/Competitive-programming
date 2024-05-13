52aec85bfd0568ef6d6616acc50c1264bddd2e65
Ameer Hamzah Daiyan <133146759+ameerhamzahd@users.noreply.github.com>
2024-05-14 00:18:53 +0600

    Add files via upload

---
+++ Good Kid.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n, mul = 1;
+        cin >> n;
+        int a[n];
+
+        for(int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        sort(a, a + n);
+        int x = *min_element(a, a + n);
+
+        a[0] = x + 1;
+
+        for(int i = 0; i < n; i++)
+        {
+            mul = mul * a[i];
+        }
+
+        cout << mul << endl;
+    }
+
+    return 0;
+}
