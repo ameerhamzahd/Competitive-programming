fc548978c6fe71d6c68c635f5ac1ed564273f0d3
Ameer Hamzah Daiyan <133146759+ameerhamzahd@users.noreply.github.com>
2024-06-06 23:33:30 +0600

    Add files via upload

---
+++ A_Domino_piling.cpp
@@ -0,0 +1,24 @@
+/*
+----------------------------------------
+     السلام عليكم ورحمة الله وبركاته
+    بِسْــــــــمِ اٌللَّهِ اٌلرَّحْمَنِ اٌلرَّحِيْـــــــــمِ
+    1% CHANCE, 99% FAITH
+            ameerhamzah.d
+----------------------------------------
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, temp, ans;
+    cin >> a >> b;
+
+    temp = a * b;
+    ans = temp / 2;
+
+    cout << ans << endl;
+
+    return 0;
+}
