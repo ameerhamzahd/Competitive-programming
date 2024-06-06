eb63478c7abcc023c3d500bb5551ca3e53a5c60f
Ameer Hamzah Daiyan <133146759+ameerhamzahd@users.noreply.github.com>
2024-06-07 00:08:38 +0600

    Add files via upload

---
+++ A_Elephant.cpp
@@ -0,0 +1,27 @@
+/*
+----------------------------------------
+     السلام عليكم ورحمة الله وبركاته
+    بِسْــــــــمِ اٌللَّهِ اٌلرَّحْمَنِ اٌلرَّحِيْـــــــــمِ
+    1% CHANCE, 99% FAITH
+            ameerhamzah.d
+----------------------------------------
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, counter = 0;
+    cin >> a;
+
+    while(a > 0)
+    {
+        a = a - 5;
+        counter++;
+    }
+
+    cout << counter << endl;
+
+    return 0;
+}
