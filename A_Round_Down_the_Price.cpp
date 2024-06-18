2b4874649c18e5f57c5ccde8d2f30072f9b952b9
Ameer Hamzah Daiyan <133146759+ameerhamzahd@users.noreply.github.com>
2024-06-18 19:21:21 +0600

    Add files via upload

---
+++ A_Round_Down_the_Price.cpp
@@ -0,0 +1,31 @@
+/*
+----------------------------------------
+     السلام عليكم ورحمة الله وبركاته
+    بِسْــــــــمِ اٌللَّهِ اٌلرَّحْمَنِ اٌلرَّحِيْـــــــــمِ
+    1% CHANCE, 99% FAITH
+            ameerhamzah.d
+----------------------------------------
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        int n, ans;
+        n = stoi(s);
+
+        ans = n - pow(10, s.size() - 1);
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
