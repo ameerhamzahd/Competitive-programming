def1c824d9efcc733dc6c4ff888d04585fbcd54d
Ameer Hamzah Daiyan <133146759+ameerhamzahd@users.noreply.github.com>
2024-06-25 00:22:15 +0600

    Add files via upload

---
+++ A_In_Search_of_an_Easy_Problem.cpp
@@ -0,0 +1,39 @@
+/*
+----------------------------------------
+     السلام عليكم ورحمة الله وبركاته
+    بِسْــــــــمِ اٌللَّهِ اٌلرَّحْمَنِ اٌلرَّحِيْـــــــــمِ
+    1% CHANCE, 99% FAITH
+            ameerhamzah.d
+----------------------------------------
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, flag = 0;
+    cin >> n;
+    vector<int> v(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (v[i] == 1)
+        {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag == 1)
+        cout << "HARD" << endl;
+    else
+        cout << "EASY" << endl;
+
+    return 0;
+}
