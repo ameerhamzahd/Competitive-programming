b857d1601cfae148b00fed49fea6a3a5288e0e3d
Ameer Hamzah Daiyan <133146759+ameerhamzahd@users.noreply.github.com>
2024-08-20 00:38:51 +0600

    Add files via upload

---
+++ A_Odd_Divisor.cpp
@@ -0,0 +1,33 @@
+/*
+----------------------------------------
+     السلام عليكم ورحمة الله وبركاته
+    بِسْــــــــمِ اٌللَّهِ اٌلرَّحْمَنِ اٌلرَّحِيْـــــــــمِ
+    1% CHANCE, 99% FAITH
+            ameerhamzah.d
+----------------------------------------
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        long long n;
+        cin >> n;
+
+        if((n & (n - 1)) == 0)
+        {
+            cout << "NO" << endl;
+        }
+        else
+        {
+            cout << "YES" << endl;
+        }
+    }
+
+    return 0;
+}
