304eb40ffee465f5cfa0d445be38dc4c64831d40
Ameer Hamzah Daiyan <133146759+ameerhamzahd@users.noreply.github.com>
2024-10-27 20:19:56 +0600

    Add files via upload

---
+++ Phitron/Print_It.cpp
@@ -0,0 +1,24 @@
+/*
+----------------------------------------
+     السلام عليكم ورحمة الله وبركاته
+    بِسْــــــــمِ اٌللَّهِ اٌلرَّحْمَنِ اٌلرَّحِيْـــــــــمِ
+    1% CHANCE, 99% FAITH
+            ameerhamzah.d
+----------------------------------------
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << "I Love Assignment" << endl;
+    }
+
+    return 0;
+}
