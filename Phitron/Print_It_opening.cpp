0a1eef94ce867f92d2c91796704f0352e398dc5b
Ameer Hamzah Daiyan <133146759+ameerhamzahd@users.noreply.github.com>
2024-11-01 00:04:11 +0600

    Add files via upload

---
+++ Phitron/Print_It_opening.cpp
@@ -0,0 +1,20 @@
+/*
+----------------------------------------
+     السلام عليكم ورحمة الله وبركاته
+    بِسْــــــــمِ اٌللَّهِ اٌلرَّحْمَنِ اٌلرَّحِيْـــــــــمِ
+    1% CHANCE, 99% FAITH
+            ameerhamzah.d
+----------------------------------------
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, world! I am learning C programming language. ^_^" << endl;
+    cout << "Programming is fun and challenging. /\\/\\/\\" << endl;
+    cout << "I want to give my 100% dedication to learn!\tI will succeed one day." << endl;    
+
+    return 0;
+}
