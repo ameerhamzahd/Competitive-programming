15afbfb21fbb7eb75efec62ee4b656e7a4418804
Ameer Hamzah Daiyan <133146759+ameerhamzahd@users.noreply.github.com>
2024-11-05 00:33:30 +0600

    Add files via upload

---
+++ Phitron/Multiply.cpp
@@ -0,0 +1,23 @@
+/*
+----------------------------------------
+     السلام عليكم ورحمة الله وبركاته
+    بِسْــــــــمِ اٌللَّهِ اٌلرَّحْمَنِ اٌلرَّحِيْـــــــــمِ
+    1% CHANCE, 99% FAITH
+            ameerhamzah.d
+----------------------------------------
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long a, b, ans;
+    cin >> a >> b;
+
+    ans = a * b;
+
+    cout << ans << endl;
+
+    return 0;
+}
