bde258a76eb5e10f0099e8149855c85c05f7cd67
Ameer Hamzah Daiyan <ameerhamzahdaiyan.02@gmail.com>
2025-08-02 20:15:48 +0600

    Add files via upload

---
+++ Beecrowd/1142.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 1, b = 2, c = 3, i, n;
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        printf("%d %d %d PUM\n", a, b, c);
+
+        a = a + 4;
+        b = b + 4;
+        c = c + 4;
+    }
+
+    return 0;
+}
