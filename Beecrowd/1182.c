d789dd54acae257f77cbfff55fb4cdbc5c19bc4b
Ameer Hamzah Daiyan <ameerhamzahdaiyan.02@gmail.com>
2026-01-12 19:01:50 +0600

    Add files via upload

---
+++ Beecrowd/1182.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, n;
+    double m[12][12], sum = 0;
+    char a;
+    scanf("%d\n", &n);
+    scanf("%c\n", &a);
+
+    for(i = 0; i < 12; i++)
+    {
+        for(j = 0; j < 12; j++)
+        {
+            scanf("%lf", &m[i][j]);
+
+            if(j == n)
+                sum = sum + m[i][n];
+        }
+    }
+
+    if(a == 'S')
+        printf("%.1lf\n", sum);
+    else if(a == 'M')
+        printf("%.1lf\n", sum / 12);
+
+    return 0;
+}
+
