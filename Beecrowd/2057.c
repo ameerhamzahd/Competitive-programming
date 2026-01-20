14b89303423c464a25d37d5dbc93e2eccea113e2
Ameer Hamzah Daiyan <ameerhamzahdaiyan.02@gmail.com>
2026-01-20 22:02:32 +0600

    Add files via upload

---
+++ Beecrowd/2057.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    scanf("%d%d%d",&a,&b,&c);
+    d=a+b+c;
+    {
+        if(d==24)
+        {
+            d=0;
+        }
+        else if(d<24 && d>0)
+        {
+            d=d;
+        }
+        else if(d>24)
+        {
+            d=d-24;
+        }
+        else if(d<0)
+        {
+            d=d+24;
+        }
+    }
+    printf("%d\n",d);
+    return 0;
+}
